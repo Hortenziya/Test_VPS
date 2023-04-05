d9d111bb92278af05648cdcff11886bb819d52ff
anastasiaceban <nastcheban@gmail.com>
2023-04-05 21:08:45 +0300

    using time.perf_counter()

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import datetime
+
 from flask import Flask, request, redirect, url_for, render_template
 import os
 import time
+from datetime import datetime
+
 
 app = Flask(__name__)
 
@@ -9,22 +13,26 @@ app = Flask(__name__)
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
+        start_time = time.perf_counter()
+
         content_length = int(request.headers.get('Content-Length', 0))
         try:
             check_free_space(content_length)
         except Exception as e:
             print(e)
             return "Ooops. Something is wrong. Give me some time to fix it!"
+
         if file:
             filename = file.filename
-            start_time = time.time()
             file.save(os.path.join('static/uploads', filename))
-            end_time = time.time()
-            time_taken = round(end_time - start_time, 2)
+            end_time = time.perf_counter()
+            time_taken = round(end_time - start_time, 4)
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             return render_template(
                 'success.html',
                 filename=filename,
-                time=time_taken
+                time=time_taken,
+                date=current_time,
             )
     return render_template('index.html')
 
@@ -41,4 +49,4 @@ def check_free_space(file_size):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
